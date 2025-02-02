3a1b897a74242ff453224d2dd52aeb5659ec1f16
anshumanawasthi214 <awasthiengineer214@gmail.com>
2025-02-02 20:25:23 +0530

    longest substring without repeating characters

---
+++ Two Pointers/Sliding window/MaxConsecutiveOnes.java
@@ -0,0 +1,30 @@
+public class MaxConsecutiveOnes {
+    public static void main(String[] args) {
+        int values[]={1,1,1,0,0,0,1,1,1,0,1, 1,1,1,0}; //   - Bruteforce appr.
+        int k=2;                                                       
+        int maxlen=0;                                                
+       /*  for(int i=0;i<values.length;i++){                            
+            int zeros=0;                                             
+            for(int j=i;j<values.length;j++){                        
+                if(values[j]==0){                                    
+                    zeros++;                                             
+                }                                                    
+                if(zeros<=k){                                        
+                    maxlen=Math.max(maxlen,j-i+1);                   
+                }        
+                  else{       
+                           break;  
+                    }                                                    
+            }                                                        
+            
+
+        }
+        System.out.println(maxlen);
+        */
+
+
+
+
+        
+    }
+}
